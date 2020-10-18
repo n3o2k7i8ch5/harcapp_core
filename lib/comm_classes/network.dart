bc518c3ee02763b19f254c5afa30f4b5b6f109cc
n3o2k7i8ch5 <n3o2k7i8ch5@protonmail.com>
2020-10-18 13:52:50 +0200

    Initial commit

---
+++ lib/comm_classes/network.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:connectivity/connectivity.dart';
+
+Future<bool> isNetworkAvailable() async {
+  var result = await Connectivity().checkConnectivity();
+  return result != ConnectivityResult.none;
+}
+
+addConnectionListener(Function(bool hasConnection) onChanged){
+  return Connectivity().onConnectivityChanged.listen(
+          (ConnectivityResult result) =>
+              onChanged(result != ConnectivityResult.none)
+  );
+}
