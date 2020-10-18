aa196cca4a4927b2c5eb1a288bd6fb5f63de26a1
n3o2k7i8ch5 <n3o2k7i8ch5@protonmail.com>
2020-10-18 16:01:28 +0200

    .

--- lib/comm_widgets/app_scaffold.dart
+++ lib/comm_widgets/app_scaffold.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:harcapp_core/comm_classes/color_pack.dart';
+
+import 'app_text.dart';
 
 class AppScaffold extends StatelessWidget{
 
@@ -32,4 +35,58 @@ class AppScaffold extends StatelessWidget{
       );
   }
 
+  static void showSnackBar(BuildContext context, String text, {String buttonText:'Ok', Function(BuildContext) onButtonPressed, Color background, String tag, Duration duration: const Duration(seconds: 3)}){
+    Scaffold.of(context).hideCurrentSnackBar();
+    Scaffold.of(context).showSnackBar(
+        getSnackBar(
+          context,
+          text,
+          buttonText: buttonText,
+          onButtonPressed: () => onButtonPressed!=null?onButtonPressed(context):Scaffold.of(context).hideCurrentSnackBar(),
+          background: background,
+          tag: tag,
+          duration: duration,
+        )
+    );
+
+  }
+
+}
+
+String _snackBarTag;
+bool _isSnackbarActive = false;
+bool isSnackBarActive({String tag}) => tag==null?_isSnackbarActive:_isSnackbarActive&&tag == _snackBarTag;
+
+SnackBar getSnackBar(BuildContext context, String text, {String buttonText:'Ok', Function onButtonPressed, Color background, String tag, Duration duration: const Duration(seconds: 3)}){
+
+  return SnackBar(
+    backgroundColor: background == null? accentColor(context) : background,
+    elevation: 6.0,
+    behavior: SnackBarBehavior.fixed,
+    content: AppText(text, color: accentIcon(context)),
+    action: SnackBarAction(
+        label: buttonText,
+        textColor: accentIcon(context),
+        onPressed: onButtonPressed
+    ),//
+    duration: duration,
+  );
+}
+
+void showSnackBar(GlobalKey<ScaffoldState> key, String text, {String buttonText:'Ok', Function onButtonPressed, Color background, String tag, Duration duration: const Duration(seconds: 3)}){
+  _snackBarTag = tag;
+  _isSnackbarActive = true;
+  key.currentState
+    ..hideCurrentSnackBar()
+    ..showSnackBar(
+        getSnackBar(
+          key.currentContext,
+          text,
+          buttonText: buttonText,
+          onButtonPressed: () => onButtonPressed!=null?onButtonPressed():key.currentState.hideCurrentSnackBar(),
+          background: background,
+          tag: tag,
+          duration: duration,
+        )
+    ).closed.then((SnackBarClosedReason reason) => _isSnackbarActive = false);
 }
