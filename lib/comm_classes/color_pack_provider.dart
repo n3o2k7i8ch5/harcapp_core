2eb2b63b90e0f71a184527f0f757e7767327498a
n3o2k7i8ch5 <n3o2k7i8ch5@protonmail.com>
2021-03-08 00:04:00 +0100

    .

--- lib/comm_classes/color_pack_provider.dart
+++ lib/comm_classes/color_pack_provider.dart
@@ -2,21 +2,27 @@ import 'package:flutter/foundation.dart';
 
 import 'color_pack.dart';
 
-abstract class ColorPackProvider extends ChangeNotifier{
+class ColorPackProvider extends ChangeNotifier{
 
-  bool get isDark;
-  ColorPack get colorPackDark;
+  bool Function() isDark;
+  ColorPack _colorPackDark;
 
   ColorPack _colorPack;
 
-  ColorPack get colorPack => isDark?colorPackDark:_colorPack;
+  ColorPack get colorPack => isDark()?_colorPackDark:_colorPack;
   set colorPack(ColorPack value){
     _colorPack = value;
     notifyListeners();
   }
 
-  ColorPackProvider(ColorPack colorPack){
-    _colorPack = colorPack;
+  ColorPackProvider({
+    @required ColorPack initColorPack,
+    @required bool Function() isDark,
+    @required ColorPack colorPackDark,
+  }){
+    _colorPack = initColorPack;
+    this.isDark = isDark;
+    this._colorPackDark = colorPackDark;
   }
 
   void notify() => notifyListeners();
