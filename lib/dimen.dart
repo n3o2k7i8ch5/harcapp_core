bd63fe6c37c2d532f97db25fe4f8a4be52ddaa43
n3o2k7i8ch5 <n3o2k7i8ch5@protonmail.com>
2021-06-09 20:04:53 +0200

    .

--- lib/dimen.dart
+++ lib/dimen.dart
@@ -33,7 +33,7 @@ class Dimen{
   static const double BOTTOM_SHEET_TITLE_MARG = 20;
   static const double BOTTOM_SHEET_MARG = 16;
 
-  double viewportFraction(BuildContext context) => 1 - (2*Dimen.SIDE_MARG/MediaQuery.of(context).size.width);
+  static double viewportFraction(BuildContext context) => 1 - (2*Dimen.SIDE_MARG/MediaQuery.of(context).size.width);
 
   static const double SETTINGS_MARG = 38;
 
