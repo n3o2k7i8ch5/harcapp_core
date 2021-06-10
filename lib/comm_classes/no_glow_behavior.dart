43325da2afc86a24b6f45b7aa53bf840ebc05f35
n3o2k7i8ch5 <n3o2k7i8ch5@protonmail.com>
2021-06-10 11:30:17 +0200

    .

---
+++ lib/comm_classes/no_glow_behavior.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+
+class NoGlowBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(BuildContext context, Widget child, AxisDirection axisDirection) => child;
+}
