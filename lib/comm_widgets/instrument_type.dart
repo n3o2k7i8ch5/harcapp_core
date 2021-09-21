e67a6395a6e6bd5ee5e8071d35b91ed2f6fd5182
n3o2k7i8ch5 <n3o2k7i8ch5@protonmail.com>
2021-09-22 01:10:46 +0200

    .

--- lib/comm_widgets/instrument_type.dart
+++ lib/comm_widgets/instrument_type.dart
@@ -20,3 +20,17 @@ InstrumentType nextInstrumentType(InstrumentType type){
   return InstrumentType.GUITAR;
 
 }
+
+int instrumentTypeToInt(InstrumentType type){
+  if(type == InstrumentType.GUITAR) return 0;
+  else if(type == InstrumentType.UKULELE) return 1;
+  else if(type == InstrumentType.MANDOLIN) return 2;
+  return -1;
+}
+
+InstrumentType intToTstrumentType(int value){
+  if(value == 0) return InstrumentType.GUITAR;
+  else if(value == 1) return InstrumentType.UKULELE;
+  else if(value == 2) return InstrumentType.MANDOLIN;
+  return InstrumentType.GUITAR;
+}
