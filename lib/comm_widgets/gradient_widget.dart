3792c4e8198ba09ee2659b69b2af79f42917e01c
n3o2k7i8ch5 <n3o2k7i8ch5@protonmail.com>
2022-04-05 21:54:07 +0200

    .

--- lib/comm_widgets/gradient_widget.dart
+++ lib/comm_widgets/gradient_widget.dart
@@ -17,6 +17,7 @@ class GradientWidget extends StatelessWidget {
   final Duration? duration;
   final Curve? curve;
   final Clip? clipBehavior;
+  final Alignment alignment;
 
   const GradientWidget({
     required this.colorStart,
@@ -30,10 +31,23 @@ class GradientWidget extends StatelessWidget {
     this.duration,
     this.curve,
     this.clipBehavior,
+    this.alignment = Alignment.topLeft,
   });
 
   @override
   Widget build(BuildContext context){
+
+    Alignment oppositeAlignment;
+    if(alignment == Alignment.center) oppositeAlignment = Alignment.center;
+    else if(alignment == Alignment.topCenter) oppositeAlignment = Alignment.bottomCenter;
+    else if(alignment == Alignment.topRight) oppositeAlignment = Alignment.bottomLeft;
+    else if(alignment == Alignment.centerRight) oppositeAlignment = Alignment.centerLeft;
+    else if(alignment == Alignment.bottomRight) oppositeAlignment = Alignment.topLeft;
+    else if(alignment == Alignment.bottomCenter) oppositeAlignment = Alignment.topCenter;
+    else if(alignment == Alignment.bottomLeft) oppositeAlignment = Alignment.topRight;
+    else if(alignment == Alignment.centerLeft) oppositeAlignment = Alignment.centerRight;
+    else if(alignment == Alignment.topLeft) oppositeAlignment = Alignment.bottomRight;
+    else oppositeAlignment = Alignment.center;
 
     Widget _child = AnimatedContainer(
       clipBehavior: clipBehavior??Clip.antiAlias,
@@ -43,8 +57,8 @@ class GradientWidget extends StatelessWidget {
         shape: shape??BoxShape.rectangle,
         borderRadius: shape!=null?null:BorderRadius.circular(radius),
         gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight, // 10% of the width, so there are ten blinds.
+          begin: alignment,
+          end: oppositeAlignment, // 10% of the width, so there are ten blinds.
           colors: [colorStart, colorEnd], // whitish to gray
           tileMode: TileMode.repeated, // repeats the gradient over the canvas
         ),
