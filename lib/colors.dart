2f1dfc284c57bdb441b7088beb94418fc1823e26
dan <iwanicki.daniel@gmail.com>
2023-12-02 22:39:08 +0100

    Added colors.

--- lib/colors.dart
+++ lib/colors.dart
@@ -12,8 +12,21 @@ class AppColors{
 
   static const Color white_dark = Color.fromARGB(255, 245, 245, 245);
 
-  static const Color zhr_red = Color.fromARGB(255, 220, 0, 20);
-  static const Color rrp_red = Color.fromARGB(255, 229, 28, 34);
+  static const Color hrpGreen = Color.fromARGB(255, 128, 128, 0);
+  static const Color zhpGreen = Color.fromARGB(255, 132, 163, 17);
+  static const Color zhrRed = Color.fromARGB(255, 220, 0, 20);
+  static const Color rrpRed = Color.fromARGB(255, 229, 28, 34);
 
+  static const Color zhpTropColor = Color.fromARGB(255, 93, 47, 136);
+  static const Color zhpTropDarkColor = Color.fromARGB(255, 56, 34, 89);
+
+  static const Color metoZhpZ = Color.fromARGB(255, 248, 196, 11);
+  static const Color metoZhpH = Color.fromARGB(255, 175, 202, 11);
+  static const Color metoZhpHS = Color.fromARGB(255, 25, 64, 147);
+  static const Color metoZhpW = Color.fromARGB(255, 227, 6, 19);
+
+  static const Color rankPwdColor = Color.fromARGB(255, 50, 40, 255);
+  static const Color rankPhmColor = Color.fromARGB(255, 30, 190, 20);
+  static const Color rankHmColor = Color.fromARGB(255, 210, 0, 0);
 
 }
