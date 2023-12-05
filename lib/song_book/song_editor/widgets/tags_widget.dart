45718ada5ced55a2ff55be54de7aff5e5bec404c
dan <iwanicki.daniel@gmail.com>
2023-12-05 10:50:22 +0100

    Added padding to SongTagsWidget

--- lib/song_book/song_editor/widgets/tags_widget.dart
+++ lib/song_book/song_editor/widgets/tags_widget.dart
@@ -10,53 +10,57 @@ import '../providers.dart';
 class SongTagsWidget extends StatelessWidget{
 
   final bool linear;
+  final EdgeInsets padding;
   final void Function(List<String> tags)? onChanged;
 
-  const SongTagsWidget({this.linear= true, this.onChanged});
+  const SongTagsWidget({
+    this.linear= true,
+    this.padding = EdgeInsets.zero,
+    this.onChanged
+  });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) => Consumer<TagsProvider>(
+    builder: (context, prov, child){
 
-    return Consumer<TagsProvider>(
-      builder: (context, prov, child){
+      Function onTagTap = (String tag, bool checked){
+        if(checked) prov.remove(tag);
+        else prov.add(tag);
 
-        Function onTagTap = (String tag, bool checked){
-          if(checked) prov.remove(tag);
-          else prov.add(tag);
+        onChanged?.call(prov.checkedTags);
+      };
 
-          onChanged?.call(prov.checkedTags);
-        };
+      return Column(
+        children: [
 
-        return Column(
-          children: [
-
-            Padding(
-              padding: EdgeInsets.only(left: Dimen.ICON_MARG),
-              child: TitleShortcutRowWidget(
-                title: 'Tagi${prov.count==0?'':' (${prov.count})'}',
-                textAlign: TextAlign.start,
-              ),
+          Padding(
+            padding: EdgeInsets.only(left: Dimen.ICON_MARG),
+            child: TitleShortcutRowWidget(
+              title: 'Tagi${prov.count==0?'':' (${prov.count})'}',
+              textAlign: TextAlign.start,
             ),
+          ),
 
-            if(linear)
-              TagsWidget.linear(
-                onTagTap: onTagTap as dynamic Function(String, bool)?,
-                allTags: SongTag.ALL,
-                checkedTags: prov.checkedTags,
-                fontSize: Dimen.TEXT_SIZE_NORMAL,
-              )
-            else
-              TagsWidget.wrap(
-                onTagTap: onTagTap as dynamic Function(String, bool)?,
-                allTags: SongTag.ALL,
-                checkedTags: prov.checkedTags,
-                fontSize: Dimen.TEXT_SIZE_NORMAL,
-              )
+          if(linear)
+            TagsWidget.linear(
+              onTagTap: onTagTap as dynamic Function(String, bool)?,
+              allTags: SongTag.ALL,
+              padding: padding,
+              checkedTags: prov.checkedTags,
+              fontSize: Dimen.TEXT_SIZE_NORMAL,
+            )
+          else
+            TagsWidget.wrap(
+              onTagTap: onTagTap as dynamic Function(String, bool)?,
+              allTags: SongTag.ALL,
+              padding: padding,
+              checkedTags: prov.checkedTags,
+              fontSize: Dimen.TEXT_SIZE_NORMAL,
+            )
 
-          ],
-        );
-      },
-    );
-  }
+        ],
+      );
+    },
+  );
 
 }
