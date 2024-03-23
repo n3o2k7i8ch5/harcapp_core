35d0e0869e1af1d6cafcfaa4c5b09f17cb5a462e
dan <iwanicki.daniel@gmail.com>
2024-03-23 17:45:31 +0100

    Added common widgets

---
+++ lib/comm_widgets/sliver_child_builder_separated_delegate.dart
@@ -0,0 +1,27 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class SliverChildSeparatedBuilderDelegate extends SliverChildBuilderDelegate{
+
+  final int count;
+  final Widget Function(BuildContext, int) separatorBuilder;
+
+  SliverChildSeparatedBuilderDelegate(builder, {required this.separatorBuilder, required this.count}) : super(
+  (BuildContext context, int index) {
+      final int itemIndex = index ~/ 2;
+      if (index.isEven) {
+        return builder(context, itemIndex);
+      }
+      return separatorBuilder(context, itemIndex);
+    },
+    semanticIndexCallback: (Widget widget, int localIndex) {
+      if (localIndex.isEven)
+        return localIndex ~/ 2;
+
+      return null;
+    },
+    childCount: max(0, count * 2 - 1),
+  );
+
+}
