eb3c82a6669c6598e9fa27b6993c9ae99400cf69
Daniel Iwanicki <daniel.iwanicki@stxnext.pl>
2024-04-19 02:57:49 +0200

    Added konspekts

--- lib/harcthought/konspekts/data.dart
+++ lib/harcthought/konspekts/data.dart
@@ -67,37 +67,37 @@ List<Konspekt> allKonspekts = [
         ),
       ],
       summary: 'Leśna gra harcerska, w której drużyny mają za zadanie wyeliminować przeciwne drużyny ściągając im berety z głów.',
-      description: 'Aby forma była skuteczna, uczestnicy muszą być w nią zaangażowani i nie mieć nic przeciwko rywalizacji (częściej jest to domeną chłopców niż dziewczyn). Forma bywa kontuzjogenna.'
+      description: '<p style="text-align:justify;">Aby forma była skuteczna, uczestnicy muszą być w nią zaangażowani i nie mieć nic przeciwko rywalizacji (częściej jest to domeną chłopców niż dziewczyn). Forma bywa kontuzjogenna.'
           '<br>'
           '<br>Forma powinna być toczona na dużym terenie lesistym zapewniającym możliwość łatwego skrycia się.'
           '<br>'
-          '<br>Zachowując opisane niżej zasady gry można nieznacznie zmienić jej cel dla uczestników, np:'
+          '<br>Zachowując opisane niżej zasady gry można nieznacznie zmienić jej cel dla uczestników, np:</p>'
           '<ul>'
           '<li>'
-          'Drużyny są dzielone na <b>atakującą</b> i <b>broniącą</b>. Drużyna <b>broniąca</b> ma wyznaczony <b>teren krytyczny</b> (np. między pięcioma drzewami). Drużyna atakująca ma wyznaczonego <b>sapera</b> - osobę, która posiada przedmiot będący <b>bombą</b> (np. menażkę).'
+          '<p style="text-align:justify;">Drużyny są dzielone na <b>atakującą</b> i <b>broniącą</b>. Drużyna <b>broniąca</b> ma wyznaczony <b>teren krytyczny</b> (np. między pięcioma drzewami). Drużyna atakująca ma wyznaczonego <b>sapera</b> - osobę, która posiada przedmiot będący <b>bombą</b> (np. menażkę).'
           '<br>'
           '<br>Drużyna <b>atakująca</b> wygrywa, jeśli <b>saper</b> położy <b>bombę</b> w <b>terenie krytycznym</b>, przy czym <b>bombą</b> nie wolno rzucać.'
           '<br>'
           '<br>Drużyna <b>broniąca</b> wygrywa, jeśli zabije <b>sapera</b> lub jeśli obroni <b>teren krytyczny</b> przez 20 minut od rozpoczęcia gry.'
-          '<br>&nbsp'
+          '<br>&nbsp</p>'
           '</li>'
           '<li>'
-          'Każda drużyna ma wyznaczoną <b>królową</b>. Drużyna przegrywa, jeśli straci <b>królową</b> niezależnie od tego ile osób pozostanie na końcu przy życiu. Przegrana drużyna natychmiast kończy grę.'
+          '<p style="text-align:justify;">Każda drużyna ma wyznaczoną <b>królową</b>. Drużyna przegrywa, jeśli straci <b>królową</b> niezależnie od tego ile osób pozostanie na końcu przy życiu. Przegrana drużyna natychmiast kończy grę.</p>'
+          '</li>'
           '</ul>',
       steps: [
-
 
         KonspektStep(
             title: 'Przedstawienie zasad',
             duration: Duration(minutes: 10),
             activeForm: false,
-            content: 'Prowadzący przedstawia uczestnikom zasady gry:'
+            content: '<p style="text-align:justify;">Prowadzący przedstawia uczestnikom zasady gry:</p>'
                 '<ul>'
-                '<li>W brze biorą udział co najmniej dwie grupy. Wygrywa grupa, której w której jako ostatniej zostaną "żywi" uczestnicy.</li>'
-                '<li>Każdy uczestnik rozpoczyna mając na głowie beret. Jak uczestnik ma na głowie beret, tak długo jest "żywy".</li>'
-                '<li>Berety można (a nawet trzeba) sobie wzajemnie ściągać z głowy.</li>'
-                '<li>W grze nie wolno uderzać, gryźć, kopać, ani dotykać twarzy drugiej osoby.</li>'
-                '<li>Gra toczy się na wybrany, jasny znak (np. trzykrotny gwizdek prowadzącego).</li>'
+                '<li><p style="text-align:justify;">W brze biorą udział co najmniej dwie grupy. Wygrywa grupa, której w której jako ostatniej zostaną "żywi" uczestnicy.</p></li>'
+                '<li><p style="text-align:justify;">Każdy uczestnik rozpoczyna mając na głowie beret. Jak uczestnik ma na głowie beret, tak długo jest "żywy".</p></li>'
+                '<li><p style="text-align:justify;">Berety można (a nawet trzeba) sobie wzajemnie ściągać z głowy.</p></li>'
+                '<li><p style="text-align:justify;">W grze nie wolno uderzać, gryźć, kopać, ani dotykać twarzy drugiej osoby.</p></li>'
+                '<li><p style="text-align:justify;">Gra toczy się na wybrany, jasny znak (np. trzykrotny gwizdek prowadzącego).</p></li>'
                 '</ul>'
         ),
 
@@ -106,7 +106,7 @@ List<Konspekt> allKonspekts = [
             title: 'Podział na grupy',
             duration: Duration(minutes: 10),
             activeForm: false,
-            content: 'Prowadzący dzieli uczestników na wybraną liczbę grup. Ważne, by każda grupa charakteryzowała się podobną sprawnością ruchową.'
+            content: '<p style="text-align:justify;">Prowadzący dzieli uczestników na wybraną liczbę grup. Ważne, by każda grupa charakteryzowała się podobną sprawnością ruchową.</p>'
         ),
 
 
@@ -114,9 +114,9 @@ List<Konspekt> allKonspekts = [
             title: 'Oznaczenie grup',
             duration: Duration(minutes: 5),
             activeForm: false,
-            content: 'Każdy uczestnik zostaje oznaczenie przynależności do swojej grupy. Może to być szarfa naramienna o innym kolorze dla każdej grupy lub po prostu może to być beret o wybranym kolorze.'
-                '<br'
-                '<br>Jeżeli do gry używane są berety harcerskie, ważne jest, by nie miały one na sobie lilijek lub by berety były "wywrócone na lewą stronę" - inaczej łatwo o pocięte dłonie.'
+            content: '<p style="text-align:justify;">Każdy uczestnik zostaje oznaczenie przynależności do swojej grupy. Może to być szarfa naramienna o innym kolorze dla każdej grupy lub po prostu może to być beret o wybranym kolorze.</p>'
+                '<br>'
+                '<br><p style="text-align:justify;">Jeżeli do gry używane są berety harcerskie, ważne jest, by nie miały one na sobie lilijek lub by berety były "wywrócone na lewą stronę" - inaczej łatwo o pocięte dłonie.</p>'
         ),
 
 
@@ -124,7 +124,7 @@ List<Konspekt> allKonspekts = [
             title: 'Gra',
             duration: Duration(minutes: 30),
             activeForm: true,
-            content: 'Grupy rozchodzą się po lesie i rozpoczyna się gra zgodnie z opisanymi wcześniej zasadami.'
+            content: '<p style="text-align:justify;">Grupy rozchodzą się po lesie i rozpoczyna się gra zgodnie z opisanymi wcześniej zasadami.</p>'
         ),
 
 
@@ -132,7 +132,7 @@ List<Konspekt> allKonspekts = [
             title: 'Podsumowanie',
             duration: Duration(minutes: 5),
             activeForm: false,
-            content: 'Prowadzący po zakończeniu gry podsumowuje jej przebieg z uczestnikami.'
+            content: '<p style="text-align:justify;">Prowadzący po zakończeniu gry podsumowuje jej przebieg z uczestnikami.</p>'
         ),
 
 
@@ -140,7 +140,7 @@ List<Konspekt> allKonspekts = [
             title: 'Rewanż',
             duration: Duration(minutes: 30),
             activeForm: true,
-            content: 'Grupy rozchodzą się po lesie i rozpoczyna się druga gra zgodnie z opisanymi wcześniej zasadami.'
+            content: '<p style="text-align:justify;">Grupy rozchodzą się po lesie i rozpoczyna się druga gra zgodnie z opisanymi wcześniej zasadami.</p>'
         ),
 
 
@@ -148,9 +148,8 @@ List<Konspekt> allKonspekts = [
             title: 'Podsumowanie',
             duration: Duration(minutes: 5),
             activeForm: false,
-            content: 'Prowadzący po zakończeniu rewanżu podsumowuje jej przebieg z uczestnikami.'
+            content: '<p style="text-align:justify;">Prowadzący po zakończeniu rewanżu podsumowuje jej przebieg z uczestnikami.</p>'
         ),
-
 
       ]
   ),
