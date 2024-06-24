94315536d2cb7ea0f7e77e3ae07218c79c270411
Daniel Iwanicki <daniel.iwanicki@stxnext.pl>
2024-06-24 02:14:40 +0200

    Fixed

--- lib/harcthought/konspekts/data/common.dart
+++ lib/harcthought/konspekts/data/common.dart
@@ -1,4 +1,4 @@
-String piramidaDuchowosciHtmlLight = '<img src="asset:packages/harcapp_core/assets/konspekty/common/warsztaty_duchowe/piramida_duchowosci_light.svg" style="width: 100%; height: auto;" alt="Piramida duchowości"/>';
-String piramidaDuchowosciHtmlDark = '<img src="asset:packages/harcapp_core/assets/konspekty/common/warsztaty_duchowe/piramida_duchowosci_dark.svg" style="width: 100%; height: auto;" alt="Piramida duchowości"/>';
+String piramidaDuchowosciHtmlLight = '<img src="asset:packages/harcapp_core/assets/konspekty/common/warsztaty_duchowe/piramida_duchowosci_light.svg" style="object-fit: contain; width: 100%; height: auto;" alt="Piramida duchowości"/>';
+String piramidaDuchowosciHtmlDark = '<img src="asset:packages/harcapp_core/assets/konspekty/common/warsztaty_duchowe/piramida_duchowosci_dark.svg" style="object-fit: contain; width: 100%; height: auto;" alt="Piramida duchowości"/>';
 
 String piramidaDuchowosciHtml({required bool isDark}) => isDark?piramidaDuchowosciHtmlDark:piramidaDuchowosciHtmlLight;
