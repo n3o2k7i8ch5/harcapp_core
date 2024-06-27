9a854d4082deb53de78eccc5b7697817eef26cb7
Daniel Iwanicki <daniel.iwanicki@stxnext.pl>
2024-06-27 18:31:37 +0200

    Fixed

--- lib/harcthought/konspekts/data/ksztalcenie/wstep_do_wychowania_duchowego.dart
+++ lib/harcthought/konspekts/data/ksztalcenie/wstep_do_wychowania_duchowego.dart
@@ -216,5 +216,103 @@ Konspekt wstep_do_wychowania_duchowego = Konspekt(
               '</p>'
       ),
 
+      KonspektStep(
+          title: 'Duchowość powszechna, mądrość, kultura i tradycja',
+          duration: Duration(minutes: 15),
+          activeForm: false,
+          content: '<p style="text-align:justify;">'
+              'Prowadzący na podstawie załącznika $attach_html_o_strukturze_i_ksztaltowaniu_duchowosci opisuje zjawisko <b>duchowości powszechnej</b>, związanej z nią <b>sztafetowością</b> i <b>selekcją naturalną</b>. Następnie definiuje w oparciu o duchowość powszechną pojęcie <b>mądrości</b> oraz jej implementacją i formą przekazu - <b>kulturą</b> i jej elementami - <b>tradycjami</b>. Prowadzący opisuje także zjawisko dualizmu tradycji - z jednej strony jej wiecznego niedoczasu względem rzeczywistości, z drugiej jej funkcji tworzenia norm i przekazywania sprawdzonych rozwiązań nowym pokoleniom.'
+              '<br>'
+              '<br>Prowadzący może zobrazować dylemat tego <i>"jak ściśle trzymać się tradycji"</i> w sposób następujący:'
+              '<br>'
+              '<br><i>Gdybyśmy zanegowali na raz wszystkie tradycje, w ciągu jednego pokolenia wrócilibyśmy do jaskiń. Ale gdybyśmy nigdy nie podważyli żadnej tradycji, nigdy z tych jaskiń byśmy nie wyszli.</i>'
+              '</p>'
+      ),
+
+      KonspektStep(
+          title: 'Duchowość, religia, religijność - opinie uczestników',
+          duration: Duration(minutes: 10),
+          activeForm: false,
+          required: false,
+          content: '<p style="text-align:justify;">'
+              'Prowadzący zadaje publicznie pytanie:'
+              '<br>'
+              '<br><b><i>“Jaka jest relacja między duchowością, religią, a religijnością?”</i></b>.'
+              '<br>'
+              '<br>Uczestnicy indywidualnie przez kilka minut na mini-kartkach zapisują hasłowo swoje odpowiedzi, które potem będą mogli rozwinąć.'
+              '<br>'
+              '<br>Prowadzący prosi uczestników kolejno o zaprezentowanie po jednej kartce i położeniu jej na środku - prezentacja w kręgu zachodzi dopóki ktoś jeszcze ma jakąś kartkę. Jeśli jakaś myśl została już przedstawiona, nie ma potrzeby jej ponownego rozwijania - można po prostu dołożyć kartkę do już położnej.'
+              '</p>'
+      ),
+
+      KonspektStep(
+          title: 'Duchowość, religia, religijność',
+          duration: Duration(minutes: 20),
+          activeForm: false,
+          content: '<p style="text-align:justify;">'
+              'Prowadzący na podstawie załącznika $attach_html_o_strukturze_i_ksztaltowaniu_duchowosci definiuje religię:'
+              '<br>'
+              '<br>Religia jest duchowością powszechną. <b>Religia nie jest “dodatkiem” do duchowości, ale jest określoną duchowością</b> - ma określone aksjomaty i wartości, określone sposoby jej (religii jako duchowość) integracji.'
+              '<br>'
+              '<br>Warto również, by prowadzący odniósł się do popularnego lecz zupełnie nietrafionego porównania, zgodnie z którym relacja między religią, a duchowością człowieka jest zdefiniowana jako relacja “zawierania”: <b>w obszernym zbiorze “duchowości” zawiera się mniejszy zbiór “religii”</b>, oraz że część osób ma duchowość “z religią”, a część “bez religii”. Relacja ta jest błędna: religia nie jest opcjonalną częścią duchowości, ale rodzajem, sposobem duchowości. Religia jest konkretną, całą duchowością.'
+              '<br>'
+              '<br>Trafniejszą analogią jest przestrzeń, w której występuje nieskończenie wiele różnych duchowości, z których każda ma swój aksjomat, wartości, postawy, zachowania. W przestrzeni tej istnieje rodzina duchowości religijnych, wśród tych zaś, istnieje ogromna rodzina duchowości chrześcijańskich: (np. chrześcijańska duchowość ludowa, jezuicka, franciszkańska, charyzmatyczna, dominikańska, etc.). Nie można jednak z duchowości “wyjąć” elementu religijnego i dalej mieć do czynienia z duchowością, tak samo jak nie można z psa wyjąć elementu “jamnik” i dalej uważać, że jest to pies. Jamnik nie jest dodatkiem do psa, tylko jest rodzajem całego, spójnego psa.'
+              '<br>'
+              '<br>Oznacza to, że błędnym jest myślenie, że możliwe jest prowadzenie wychowania w drużynie w oparciu o jedną, wybraną duchowość, i “wzbogacania” jej dla niektórych religią, a dla innych nie. Wynika to z faktu, że jeśli ktoś jest wychowywany w duchowości religijnej, ma określone religijne aksjomaty, co stoi w zupełnym kontraście do aksjomatów osób niereligijnych!'
+              '<br>'
+              '<br>Podobnie, błędnym jest pogląd jakoby istniała automatyczna symetria, między duchowością osób religijnych i niereligijnych: osoby religijne mają z góry określone aksjomaty, jednak aksjomaty osób niereligijnych dopiero wymagają określenia i doprecyzowania.'
+              '<br>'
+              '<br><b>Religijność</b> jest zestawem zachowań i postaw wynikających z duchowości religijnej. Religijność nie jest jednak zbiorem wartości, ani całą duchowością - religijność to jedynie wierzchnia warstwa duchowości, która pozwala (lecz sama w sobie niekoniecznie wystarcza) by duchowość religijną skutecznie integrować.'
+              '</p>'
+      ),
+
+      // Add aim:
+      // - Zaprezentowanie uczestnikom wartości i postaw (przebaczenie, prawdomówność, pomoc bliźnim, wierność w związku, ew. modlitwa), które choć pozornie uniwersalne, wynikają ze ściśle określonego światopoglądu.
+      // - Przekonanie uczestników, że neutralność światopoglądowa w wychowaniu nie jest możliwa.
+      KonspektStep(
+          title: 'Neutralność światopoglądowa',
+          duration: Duration(minutes: 25),
+          activeForm: false,
+          content: '<p style="text-align:justify;">'
+              'Prowadzący przedstawia uczestnikom scenariusze z załącznika $attach_neutralnosc_duchowa_przyklady. Wszystkie scenariusze opisują sytuacje wychowawcze z udziałem instruktora harcerskiego. Zadaniem uczestników jest określenie:'
+              '<br>'
+              '<br><u>Czy zachowanie instruktora <b>było neutralne duchowo</b>, a jeśli nie, to <b>jakie wartości</b> lub postawy owa sytuacja wzmacnia?</u>'
+              '<br>'
+              '<br>Prowadzący prosi, by uczestnicy nie skupiali się na technikaliach, czyli czy instruktor zareagował w sposób efektywny. Ważniejsze jest, by uczestnicy skupili się na skutku duchowym jego działań - czyli na duchowości (postawa, wartościach), które działanie instruktora wzmocniło u wychowanków.'
+              '<br>'
+              '<br>Jeśli uczestników jest więcej niż 20, można podzielić ich na dwie grupy, które równolegle między sobą dyskutują o scenariuszach.'
+              '<br>'
+              '<br>Gdy wszystkie grupy przedyskutują swoje scenariusze, prowadzący zadaje pytanie:'
+              '<br>'
+              '<br><b><i>“Czy w ogóle istnieje neutralność światopoglądowa w wychowaniu?”</i></b>.'
+              '<br>'
+              '<br>W toku próby odpowiedzi na to pytanie może wywiązać się między uczestnikami dyskusja. Nie powinna ona trwać zbyt długo. Scenariusze, które zostały omówione przez uczestników powinny prowadzić do wniosku: w sposób oczywisty harcerskie <b>wychowanie nie jest neutralne duchowo</b>.'
+              '</p>'
+      ),
+
+      // Add aim:
+      // - Zbudowanie u uczestników zdolności wiązania wartości harcerskich z ich aksjomatami.
+      // - Przebicie bańki fałszywej tezy relatywizmu wyznawanych w harcerstwie wartości.
+      KonspektStep(
+          title: 'Źródła wartości - dyskusje o scenariuszach',
+          duration: Duration(minutes: 15),
+          activeForm: false,
+          content: '<p style="text-align:justify;">'
+              'Prowadzący zaczyna od stwierdzenia:'
+              '<br>'
+              '<br><i>“Skoro wiadomo, że harcerstwo nie jest neutralne, to spróbujmy wgryźć się w esencję ducha harcerskiego wychowania. Może uda się to zrobić bez utonięcia w filozofii i teologii”</i>.'
+              '<br>'
+              '<br>Prowadzący znowu prezentuje uczestnikom scenariusze z załącznika $attach_neutralnosc_duchowa_przyklady - przykłady”. Prowadzący stawia przed nimi zadanie w formie pytania:'
+              '<br>'
+              '<br><i>“Wszystkie przedstawione w scenariuszach wartości mają swoje odzwierciedlenie w PH. Ale przecież PH nie spadło z nieba na kamiennych tablicach! Z jakiego aksjomatu wynikają prezentowane w scenariuszu postawy, przekonania i wartości?”</i>'
+              '<br>'
+              '<br>Prowadzący prosi uczestników, by dokopali się do najgłębszego źródła wartości (aksjomatu). Uczestnicy nie powinni bać się zanurkowania w przestrzeń <b>religii</b> i <b>filozofii</b>, a czasami <b>selekcji naturalnej</b>. Grupa nie musi być zgodna we wnioskach - może zaproponować zapisać kilka różnych aksjomatów, z których wynikają omawiane wcześniej wartości.'
+              '<br>'
+              '<br>Wskazane jest, by uczestnicy mieli możliwość swobodnej dyskusji. Dlatego, jeśli to możliwe, warto podzielić uczestników na grupy po nie więcej niż 7 osób i dać im po jednym ze scenariuszy. Dodatkowo każda grupa powinna dostać kartkę, na której powinna spisać swoje wnioski.'
+              '<br>'
+              '<br>Na końcu każda grupa w ciągu minuty prezentuje wyniki swojej pracy. Gdy skończy, prowadzący prosi, by położyli omawiany scenariusz oraz kartkę z aksjomatem przed sobą.'
+              '</p>'
+      ),
+
     ]
 );
