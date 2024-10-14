e11a76c98908e553ad6e040a54655600eee9ee46
Daniel Iwanicki <daniel.iwanicki@stxnext.pl>
2024-10-14 14:36:42 +0200

    Fixed konspekts

--- lib/harcthought/konspekts/filter_widgets/konspekt_search_filters.dart
+++ lib/harcthought/konspekts/filter_widgets/konspekt_search_filters.dart
@@ -1,13 +1,17 @@
 import 'package:harcapp_core/comm_classes/meto.dart';
 import 'package:harcapp_core/harcthought/konspekts/konspekt.dart';
 
-abstract class KonspektSearchFilters{}
+class KonspektSearchFilters{
+  String phrase;
+  KonspektSearchFilters({required this.phrase});
+}
 
 class KonspektHarcerskieSearchFilters extends KonspektSearchFilters{
   final Set<Meto> selectedMetos;
   final Set<KonspektSphere> selectedSpheres;
 
   KonspektHarcerskieSearchFilters({
+    required super.phrase,
     required this.selectedMetos,
     required this.selectedSpheres,
   });
@@ -17,6 +21,7 @@ class KonspektKsztalcenioweSearchFilters extends KonspektSearchFilters{
   final Set<Meto> selectedLevels;
 
   KonspektKsztalcenioweSearchFilters({
+    required super.phrase,
     required this.selectedLevels,
   });
 }
