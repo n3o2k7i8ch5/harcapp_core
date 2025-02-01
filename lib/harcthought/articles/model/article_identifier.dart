03246bbcfd37041ad4dbb8ba2d2046e9230d7adf
Daniel Iwanicki <daniel.iwanicki@stxnext.pl>
2025-02-01 18:43:36 +0100

    Added shaPref

--- lib/harcthought/articles/model/article_identifier.dart
+++ lib/harcthought/articles/model/article_identifier.dart
@@ -1,6 +1,6 @@
 import 'article_source.dart';
 
-abstract class ArticleIdentifier{
+class ArticleIdentifier{
 
   static const String uniqNameSep = '@';
 
@@ -10,4 +10,10 @@ abstract class ArticleIdentifier{
   String get uniqName => source.name + ArticleIdentifier.uniqNameSep + localId;
 
   const ArticleIdentifier(this.localId, this.source);
+
+  static ArticleIdentifier fromUniqName(String uniqName){
+    List<String> parts = uniqName.split(uniqNameSep);
+    ArticleSource source = ArticleSource.fromName(parts[0])??(throw 'Invalid source name: ${parts[0]}');
+    return ArticleIdentifier(parts[1], source);
+  }
 }
