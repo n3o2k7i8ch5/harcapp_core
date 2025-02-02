f883edf388f5849ac8ca01a89a09e3d37f4523db
Daniel Iwanicki <daniel.iwanicki@stxnext.pl>
2025-02-02 14:19:18 +0100

    Added article widgets

--- lib/logger.dart
+++ lib/logger.dart
@@ -1,5 +1,9 @@
 import 'package:logger/logger.dart';
 
-Logger? logger;
 
-void initLogger(Logger l) => logger = l;
+Logger _fallbackLogger = Logger();
+Logger? _logger;
+
+Logger get logger => _logger??_fallbackLogger;
+
+void initLogger(Logger l) => _logger = l;
