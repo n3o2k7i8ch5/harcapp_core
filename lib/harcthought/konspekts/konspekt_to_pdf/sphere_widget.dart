eaae9f3d12097c6304ecc903914f25a96bf242e7
Daniel Iwanicki <daniel.iwanicki@stxnext.pl>
2025-02-22 14:27:22 +0100

    Fixed konspekt

--- lib/harcthought/konspekts/konspekt_to_pdf/sphere_widget.dart
+++ lib/harcthought/konspekts/konspekt_to_pdf/sphere_widget.dart
@@ -27,61 +27,56 @@ Widget SphereDetailFactorWidget(String detail, Set<KonspektSphereFactor>? factor
   ],
 );
 
-Widget SphereDetailLevelWidget(KonspektSphereLevel level, Map<String, Set<KonspektSphereFactor>?> data, Font font, Font fontBold){
+List<Widget> SphereDetailLevelWidget(KonspektSphereLevel level, Map<String, Set<KonspektSphereFactor>?> data, Font font, Font fontBold) =>  [
+  if(level != KonspektSphereLevel.other)
+    level.pdfWidget(fontBold, defTextSize),
 
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.stretch,
-    children: [
-      if(level != KonspektSphereLevel.other)
-        level.pdfWidget(fontBold, defTextSize),
+  if(level != KonspektSphereLevel.other)
+    SizedBox(height: .5*defMarg),
 
-      if(level != KonspektSphereLevel.other)
-        SizedBox(height: .5*defMarg),
+  ...spaceWidgets(
+    spacing: defMarg,
+    children: data.keys.map((detail) => Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
 
-      ...spaceWidgets(
-        spacing: defMarg,
-        children: data.keys.map((detail) => Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 2.0),
+          child: Icon(IconData(MdiIcons.circleMedium.codePoint), size: defTextSize),
+        ),
 
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 2.0),
-              child: Icon(IconData(MdiIcons.circleMedium.codePoint), size: defTextSize),
-            ),
+        Expanded(
+          child: SphereDetailFactorWidget(detail, data[detail], font, fontBold),
+        ),
+      ],
+    )).toList(),
+  )
+];
 
-            Expanded(
-              child: SphereDetailFactorWidget(detail, data[detail], font, fontBold),
-            ),
-          ],
-        )).toList(),
-      )
-    ],
-  );
-  
-}
-
-Widget SphereDetailsWidget(KonspektSphereDetails details, Font font, Font fontBold){
+List<Widget> SphereDetailsWidget(KonspektSphereDetails details, Font font, Font fontBold){
 
   if(details.levels == null || details.levels!.isEmpty)
-    return Container();
+    return [Container()];
 
-  return Padding(
-    padding: const EdgeInsets.all(2*defMarg),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: spaceWidgets(
-        spacing: 2*defMarg,
-        children: details.levels!.keys.map((level) =>
-          SphereDetailLevelWidget(
-            level,
-            details.levels![level]!,
-            font,
-            fontBold
-          )
-        ).toList(),
+  List<Widget> result = [];
+
+  result.add(SizedBox(height: 2*defMarg));
+  for(MapEntry entry in details.levels!.entries){
+    KonspektSphereLevel level = entry.key;
+    Map<String, Set<KonspektSphereFactor>?> data = entry.value;
+
+    List<Widget> children = SphereDetailLevelWidget(level, data, font, fontBold);
+    children = children.map(
+      (child) => Padding(
+        padding: EdgeInsets.symmetric(horizontal: 2*defMarg),
+        child: child
       )
-    ),
-  );
+    ).toList();
+    result.addAll(children);
+  }
+  result.add(SizedBox(height: 2*defMarg));
+
+  return result;
 
 }
 
@@ -118,9 +113,9 @@ Widget SphereWidget(KonspektSphere sphere, KonspektSphereDetails? details, Font 
       SizedBox(height: 2*defMarg),
 
       if(details != null && sphere == KonspektSphere.duch)
-        SphereDetailsWidget(details, font, fontBold)
+        ...SphereDetailsWidget(details, font, fontBold)
       else if(details != null)
-        SphereDetailsWidget(details, font, fontBold)
+        ...SphereDetailsWidget(details, font, fontBold)
 
     ],
   ),
