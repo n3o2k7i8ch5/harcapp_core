577175278fc061f20f94c2ee8e226f35f3e70005
dan <iwanicki.daniel@gmail.com>
2025-07-23 11:33:14 +0200

    Fixed konspekts

--- lib/comm_classes/app_navigator.dart
+++ lib/comm_classes/app_navigator.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-Duration pageTransDuration = MaterialPageRoute<Scaffold>(
+Duration get pageTransDuration => MaterialPageRoute<Scaffold>(
     builder: (context) => Container()
 ).transitionDuration;
 
