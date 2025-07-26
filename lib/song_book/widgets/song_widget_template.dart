0dc964483f7b11e937b25096c678c484b16113eb
Daniel Iwanicki <daniel.iwanicki@stxnext.pl>
2025-07-26 14:55:53 +0200

    values update

--- lib/song_book/widgets/song_widget_template.dart
+++ lib/song_book/widgets/song_widget_template.dart
@@ -1183,7 +1183,7 @@ class TextSizeIcon extends StatelessWidget{
               alignment: Alignment.bottomCenter,
               child: Text('A', style: TextStyle(
                   height: 1,
-                  fontSize: 23.0,
+                  fontSize: 21.0,
                   color: iconEnab_(context),
                   fontWeight: FontWeight.w400,
                   fontFamily: 'Roboto'
@@ -1194,7 +1194,7 @@ class TextSizeIcon extends StatelessWidget{
               alignment: Alignment.bottomCenter,
               child: Text('A', style: TextStyle(
                   height: 1.1,
-                  fontSize: 16.0,
+                  fontSize: 15.0,
                   color: iconEnab_(context),
                   fontWeight: FontWeight.w500,
                   fontFamily: 'Roboto'
