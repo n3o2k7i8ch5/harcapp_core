d06787b5815eab650dbb8c388945610203e4e609
Daniel Iwanicki <daniel.iwanicki@stxnext.pl>
2025-08-03 10:53:33 +0200

    values update

--- lib/harcthought/konspekts/filter/konspekt_filters_widget.dart
+++ lib/harcthought/konspekts/filter/konspekt_filters_widget.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'package:harcapp_core/comm_classes/meto.dart';
-import 'package:harcapp_core/comm_widgets/app_card.dart';
-import 'package:harcapp_core/comm_widgets/meto.dart';
-import 'package:harcapp_core/comm_widgets/simple_button.dart';
 import 'package:harcapp_core/comm_widgets/title_show_row_widget.dart';
 import 'package:harcapp_core/values/dimen.dart';
 import 'package:harcapp_core/harcthought/konspekts/konspekt.dart';
 import 'package:harcapp_core/tag/tags_widget.dart';
 
+import '../widgets/level_selectable_grid_widget.dart';
 import 'konspekt_filters.dart';
 
 abstract class KonspektFiltersWidget<T extends KonspektFilters> extends StatefulWidget{
@@ -142,89 +139,3 @@ class KonspektKsztalcenieFiltersWidgetState extends State<KonspektKsztalcenieFil
 
 /// ---
 
-class LevelSelectableGridWidget extends StatelessWidget{
-
-  final Set<Meto> availableLevels;
-  final Set<Meto> selectedLevels;
-  final bool oneLine;
-  final void Function(Meto meto, bool checked)? onLevelTap;
-
-  const LevelSelectableGridWidget(this.availableLevels, this.selectedLevels, {this.oneLine = false, this.onLevelTap, super.key});
-
-  @override
-  Widget build(BuildContext context){
-
-    List<Widget> children = [];
-
-    for(Meto meto in availableLevels){
-      children.add(
-
-        _AnimatedSelectedWrapper(
-          enabled: selectedLevels.contains(meto),
-          onTap: () => onLevelTap?.call(meto, selectedLevels.contains(meto)),
-          child: MetoTile(
-            meto: meto,
-            iconSize: 42.0,
-            trailing: Padding(
-              padding: EdgeInsets.only(right: 8),
-              child: Icon(
-                selectedLevels.contains(meto)?Icons.check_circle:Icons.radio_button_unchecked,
-                color: Colors.white,
-              ),
-            ),
-            intrinsicWidth: false,
-          ),
-        ),
-
-      );
-    }
-
-    if(oneLine)
-      return SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          clipBehavior: Clip.none,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: children,
-          )
-      );
-
-    return LayoutGrid(
-      columnSizes: [1.fr, 1.fr],
-      rowSizes: List.filled((availableLevels.length/2).ceil(), auto),
-      columnGap: Dimen.defMarg,
-      rowGap: Dimen.defMarg,
-      children: children,
-    );
-
-  }
-
-}
-
-class _AnimatedSelectedWrapper extends StatelessWidget{
-
-  final bool enabled;
-  final Widget child;
-  final void Function()? onTap;
-
-  const _AnimatedSelectedWrapper({
-    required this.enabled,
-    required this.child,
-    this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) => Opacity(
-    opacity: enabled?1:0.5,
-    child: SimpleButton(
-      clipBehavior: Clip.hardEdge,
-      color: Colors.transparent,
-      elevation: enabled?AppCard.bigElevation:0,
-      borderRadius: BorderRadius.circular(AppCard.defRadius),
-      onTap: onTap,
-      child: child,
-    ),
-  );
-
-}
