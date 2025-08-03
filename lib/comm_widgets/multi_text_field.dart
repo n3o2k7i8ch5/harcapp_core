f30733a5536c0ffcfd4a95eb13d4f0cfef0e9713
Daniel Iwanicki <daniel.iwanicki@stxnext.pl>
2025-08-03 16:34:55 +0200

    values update

--- lib/comm_widgets/multi_text_field.dart
+++ lib/comm_widgets/multi_text_field.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:flutter/material.dart';
 import 'package:harcapp_core/comm_classes/app_text_style.dart';
 import 'package:harcapp_core/comm_classes/color_pack.dart';
