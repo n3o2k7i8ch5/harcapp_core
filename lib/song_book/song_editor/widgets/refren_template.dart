4d80d586efb449b78baa2eeb0ad4288e4f512ae0
Daniel Iwanicki <daniel.iwanicki@stxnext.pl>
2025-08-31 12:43:56 +0200

    Song stuff

--- lib/song_book/song_editor/widgets/refren_template.dart
+++ lib/song_book/song_editor/widgets/refren_template.dart
@@ -3,7 +3,6 @@ import 'package:harcapp_core/comm_classes/color_pack.dart';
 import 'package:harcapp_core/comm_widgets/app_button.dart';
 import 'package:harcapp_core/comm_widgets/app_scaffold.dart';
 import 'package:harcapp_core/comm_widgets/title_show_row_widget.dart';
-import 'package:harcapp_core/values/dimen.dart';
 import 'package:provider/provider.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
@@ -24,36 +23,33 @@ class RefrenTemplate extends StatelessWidget{
     builder: (context, currItemProv, refProv, child) => SongPartCard(
       songPart: currItemProv.song.refrenPart,
       type: SongPartType.REFREN_TEMPLATE,
-      topBuilder: (context, part) => Padding(
-        padding: EdgeInsets.only(left: Dimen.iconMarg - Dimen.defMarg),
-        child: Consumer<CurrentItemProvider>(
-          builder: (context, currItemProv, child) => TitleShortcutRowWidget(
-              title: 'Szablon refrenu',
-              titleColor:
-              currItemProv.hasRefren?textEnab_(context):textDisab_(context),
-              textAlign: TextAlign.left,
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
+      topBuilder: (context, part) => Consumer<CurrentItemProvider>(
+        builder: (context, currItemProv, child) => TitleShortcutRowWidget(
+            title: 'Szablon refrenu',
+            titleColor:
+            currItemProv.hasRefren?textEnab_(context):textDisab_(context),
+            textAlign: TextAlign.left,
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
 
-                  if(currItemProv.song.refrenPart.isError)
-                    AppButton(
-                      icon: Icon(MdiIcons.alertCircleOutline, color: Colors.red),
-                      onTap: () => AppScaffold.showMessage(context, text: 'Refren nie spełnia standardów. Podejrzyj go by dowiedzieć się więcej.'),
-                    ),
-
-                  Switch(
-                    value: currItemProv.hasRefren,
-                    onChanged: (bool value){
-                      currItemProv.setHasRefren(!currItemProv.hasRefren);
-                      if(onRefrenEnabledChanged != null) onRefrenEnabledChanged!(value);
-                    },
-                    activeThumbColor: accentColor??accent_(context),
+                if(currItemProv.song.refrenPart.isError)
+                  AppButton(
+                    icon: Icon(MdiIcons.alertCircleOutline, color: Colors.red),
+                    onTap: () => AppScaffold.showMessage(context, text: 'Refren nie spełnia standardów. Podejrzyj go by dowiedzieć się więcej.'),
                   ),
 
-                ],
-              )
-          ),
+                Switch(
+                  value: currItemProv.hasRefren,
+                  onChanged: (bool value){
+                    currItemProv.setHasRefren(!currItemProv.hasRefren);
+                    if(onRefrenEnabledChanged != null) onRefrenEnabledChanged!(value);
+                  },
+                  activeThumbColor: accentColor??accent_(context),
+                ),
+
+              ],
+            )
         ),
       ),
       onTap: () => onPartTap?.call(),
