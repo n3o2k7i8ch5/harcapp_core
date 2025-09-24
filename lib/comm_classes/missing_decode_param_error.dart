7467b8c0dddd234a4edd1b7894293cb73948db41
Daniel Iwanicki <daniel.iwanicki@stxnext.pl>
2025-09-24 08:54:31 +0200

    Konspekt stuff

--- lib/comm_classes/missing_decode_param_error.dart
+++ lib/comm_classes/missing_decode_param_error.dart
@@ -8,3 +8,15 @@ class MissingDecodeParamError extends Error{
   String toString() => 'MissingDecodeParamError. Missing param: $paramName.';
 
 }
+
+class InvalidDecodeParamError extends Error{
+
+  String paramName;
+  dynamic paramValue;
+
+  InvalidDecodeParamError(this.paramName, this.paramValue);
+
+  @override
+  String toString() => 'InvalidDecodeParamError. Invalid value: "$paramValue" for param: "$paramName".';
+
+}
