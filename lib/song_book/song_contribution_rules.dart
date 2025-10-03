54a53ae3081f0008f06e6a65c03b198e9d8d5d45
daniel <daniel.iwanicki@armlab.ai>
2025-10-04 00:43:13 +0200

    Fixes

--- lib/song_book/song_contribution_rules.dart
+++ lib/song_book/song_contribution_rules.dart
@@ -5,6 +5,33 @@ String get latestSongContributionRules => songContributionRules.values.first;
 
 // Newest at top, oldest at bottom
 Map<String, String> songContributionRules = {
+  'v05.10.2025':
+  'Zasady dodawania piosenek do śpiewnika aplikacji Harcapp z dnia 4. października 2025 A.D.'
+  '\n'
+  '\n<b>1. Kontrybucja</b>'
+  '\nŚpiewnik aplikacji HarcApp jest tworzony przez użytkowników, dla użytkowników. Każdy użytkownik może dodać swoją piosenkę, która będzie widoczna dla innych użytkowników.'
+  '\n'
+  '\n<b>2. Przesyłanie piosenek</b>'
+  '\nKażdy użytkownik może przesłać propozycję piosenki do śpiewnika aplikacji HarcApp w formacie .hrcpsng na adres e-mail: <b>harcapp@gmail.com</b>.'
+  '\n'
+  '\n<b>3. Weryfikacja piosenek</b>'
+  '\nKażda piosenka po przesłaniu, jest weryfikowana przez zespół HarcApp. Weryfikacja obejmuje sprawdzenie poprawności formatu przesłanej piosenki, treści oraz zgodności z wartościami harcerskimi. Propozycje piosenek, które są już dostępne w śpiewniku aplikacji HarcApp nie będą rozpatrywane.'
+  '\n'
+  '\n<b>4. Zgłaszanie uwag i poprawek</b>'
+  '\nUżytkownicy mogą zgłaszać uwagi i poprawki do przesłanych piosenek swoich lub cudzych na adres e-mail: <b>harcapp@gmail.com</b>. Zgłoszenie może zostać dokonane w postaci opisu uwagi lub w postaci przesłania poprawionej wersji piosenki. Zgłoszenia są rozpatrywane przez zespół HarcApp.'
+  '\n'
+  '\n<b>5. Czas rozpatrywania zgłoszeń</b>'
+  '\nAplikacja HarcApp, w tym także jej śpiewnik, jest tworzona przez wolontariuszy pracujących "po godzinach". Zespół HarcApp nie jest w stanie każdorazowo zagwarantować szybkiej odpowiedzi na złoszenia. Dołożone są wszelkie starania, by zgłoszenia były sukcesywnie obsługiwane.'
+  '\n'
+  '\n<b>6. Dane osoby dodającej</b>'
+  '\nPrzesyłając piosenkę do śpiewnika aplikacji HarcApp, użytkownik ma możliwość podania swoich danych osobowych w celu zidentyfikowania siebie jako osoby dodającej piosenkę. Podając osobę dodającą użytkownik zobligowany jest podać swoje dane. Podanie danych osoby dodającej jest równoznaczne ze zgodą na publiczne udostępnianie tych danych w aplikacji HarcApp.'
+  '\n'
+  '\n<b>7. Prawa autorskie</b>'
+  '\nPrzesyłając piosenkę do śpiewnika aplikacji HarcApp, użytkownik oświadcza, że jest jej autorem lub posiada prawa do jej publikacji. Użytkownik wyraża zgodę na publikację piosenki w aplikacji HarcApp oraz na jej udostępnianie innym użytkownikom. W przypadku stwierdzenia naruszenia praw autorskich, zespół HarcApp zastrzega sobie prawo do usunięcia piosenki ze śpiewnika.'
+  '\n'
+  '\n<b>8. Stwierdzenie naruszeń</b>'
+  '\nW przypadku stwierdzenia naruszenia prawa, w szczególności praw autorskich, zespół HarcApp zastrzega sobie prawo do usunięcia piosenki ze śpiewnika. Naruszenia praw autorskich mogą być zgłaszane na adres e-mail: <b>harcapp@gmail.com</b><b>',
+
   'v19.08.2025':
   'Zasady dodawania piosenek do śpiewnika aplikacji Harcapp z dnia 19. sierpnia 2025 A.D.'
   '\n'
@@ -21,7 +48,7 @@ Map<String, String> songContributionRules = {
   '\nUżytkownicy mogą zgłaszać uwagi i poprawki do przesłanych piosenek swoich lub cudzych na adres e-mail: <b>harcapp@gmail.com</b>. Zgłoszenie może zostać dokonane w postaci opisu uwagi lub w postaci przesłania poprawionej wersji piosenki. Zgłoszenia są rozpatrywane przez zespół HarcApp.'
   '\n'
   '\n<b>5. Czas rozpatrywania zgłoszeń</b>'
-  '\nAplikacja HarcApp, w tym także jej śpiewnik, jest tworzona przez wolontariuszy pracujących "po godzinach". Zespół HarcApp nie jest w stanie każdorazowo zagwarantować szybkiej odpowiedzi na złoszenia. Dołożone są wszelkie starania, by zgłoszenia były sukcesywnie obsługiwane.'
+  '\nAplikacja HarcApp, w tym także jej śpiewnik, jest tworzona przez wolontariuszy pracujących "po godzinach". Zespół HarcApp nie jest w stanie każdorazowo zagwarantować szybkiej odpowiedzi na zgłoszenia. Dołożone są wszelkie starania, by zgłoszenia były sukcesywnie obsługiwane.'
   '\n'
   '\n<b>6. Prawa autorskie</b>'
   '\nPrzesyłając piosenkę do śpiewnika aplikacji HarcApp, użytkownik oświadcza, że jest jej autorem lub posiada prawa do jej publikacji. Użytkownik wyraża zgodę na publikację piosenki w aplikacji HarcApp oraz na jej udostępnianie innym użytkownikom. W przypadku stwierdzenia naruszenia praw autorskich, zespół HarcApp zastrzega sobie prawo do usunięcia piosenki ze śpiewnika.'
