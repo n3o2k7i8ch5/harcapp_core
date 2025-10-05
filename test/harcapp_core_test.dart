1df412cb839b16374a67703a9a7d333aebd77202
daniel <daniel.iwanicki@armlab.ai>
2025-10-06 00:05:52 +0200

    Fixes

--- test/harcapp_core_test.dart
+++ test/harcapp_core_test.dart
@@ -1,20 +1,3 @@
-import 'package:harcapp_core/sprawnosci/models.dart';
-import 'package:isar_community/isar.dart';
-
-late Isar isar;
-
-Future<void> initIsar() async {
-  isar = await Isar.open(
-    [SprawBookSchema, SprawGroupSchema, SprawFamilySchema, SprawItemSchema],
-    directory: 'assets/sprawnosci_db.isar',
-  );
-}
-
-void main() async {
+void main() {
   "XD";
-
-  await Isar.initializeIsarCore(download: true);
-  await initIsar();
-  final sprawBook = await isar.sprawBooks.getBySlug('zhp_harc_sim_2022');
-  print(sprawBook?.name);
 }
