aca33cff15c1acf79c73c691ee54877a3d18031f
daniel <daniel.iwanicki@armlab.ai>
2025-10-06 00:20:42 +0200

    Fixes

---
+++ lib/isar_utils.dart
@@ -0,0 +1,12 @@
+import 'package:harcapp_core/sprawnosci/models.dart';
+import 'package:isar_community/isar.dart';
+
+late Isar isar;
+
+Future<void> initIsar() async {
+  await Isar.initializeIsarCore(download: true);
+  isar = await Isar.open(
+    [SprawBookSchema, SprawGroupSchema, SprawFamilySchema, SprawItemSchema],
+    directory: 'assets/sprawnosci_db.isar',
+  );
+}
