4fc6f2b545d1c90ce421374e9c04300db67b6b6e
daniel <daniel.iwanicki@armlab.ai>
2025-10-06 13:35:07 +0200

    Fixes

--- lib/sprawnosci/all_spraw_book_slugs.dart
+++ lib/sprawnosci/all_spraw_book_slugs.dart
@@ -2,7 +2,7 @@ enum SprawBookSlug {
   zhpZuchSim2022,
   zhpHarcSim2022,
   zhrHarcCSim2023,
-  zhrHarcSim2023;
+  zhrHarcDSim2023;
 
   String get name{
     switch(this){
@@ -12,7 +12,7 @@ enum SprawBookSlug {
         return 'zhp_harc_sim_2022';
       case SprawBookSlug.zhrHarcCSim2023:
         return 'zhr_harc_c_sim_2023';
-      case SprawBookSlug.zhrHarcSim2023:
+      case SprawBookSlug.zhrHarcDSim2023:
         return 'zhr_harc_d_sim_2023';
     }
   }
@@ -26,7 +26,7 @@ enum SprawBookSlug {
       case 'zhr_harc_c_sim_2023':
         return SprawBookSlug.zhrHarcCSim2023;
       case 'zhr_harc_d_sim_2023':
-        return SprawBookSlug.zhrHarcSim2023;
+        return SprawBookSlug.zhrHarcDSim2023;
       default:
         return null;
     }
