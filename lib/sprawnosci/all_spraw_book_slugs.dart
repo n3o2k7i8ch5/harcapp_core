f13b1ec9e4ba969d2aa1f645daa1a1cb399f55f2
daniel <daniel.iwanicki@armlab.ai>
2025-10-09 10:17:32 +0200

    Fixes

--- lib/sprawnosci/all_spraw_book_slugs.dart
+++ lib/sprawnosci/all_spraw_book_slugs.dart
@@ -14,18 +14,18 @@ enum SprawBookSlug {
 
   String get name{
     switch(this){
-      case SprawBookSlug.zhpZuchSim2022:
+      case zhpZuchSim2022:
         return 'zhp_zuch_sim_2022';
-      case SprawBookSlug.zhpHarcSim2022:
+      case zhpHarcSim2022:
         return 'zhp_harc_sim_2022';
-      case SprawBookSlug.zhrHarcCSim2023:
+      case zhrHarcCSim2023:
         return 'zhr_harc_c_sim_2023';
-      case SprawBookSlug.zhrHarcDSim2023:
+      case zhrHarcDSim2023:
         return 'zhr_harc_d_sim_2023';
       case zhrHarcDSim2006:
         return 'zhr_harc_d_sim_2006';
       case zhpHarcSim2003:
-        return 'zhr_harc_d_sim_2003';
+        return 'zhp_harc_sim_2003';
       case zhpHarcSim2003Wodne:
         return 'zhp_harc_sim_2003_wodne';
     }
@@ -34,17 +34,17 @@ enum SprawBookSlug {
   static SprawBookSlug? fromName(String name){
     switch(name){
       case 'zhp_zuch_sim_2022':
-        return SprawBookSlug.zhpZuchSim2022;
+        return zhpZuchSim2022;
       case 'zhp_harc_sim_2022':
-        return SprawBookSlug.zhpHarcSim2022;
+        return zhpHarcSim2022;
       case 'zhr_harc_c_sim_2023':
-        return SprawBookSlug.zhrHarcCSim2023;
+        return zhrHarcCSim2023;
       case 'zhr_harc_d_sim_2023':
-        return SprawBookSlug.zhrHarcDSim2023;
+        return zhrHarcDSim2023;
       case 'zhr_harc_d_sim_2006':
-        return SprawBookSlug.zhrHarcDSim2006;
+        return zhrHarcDSim2006;
       case 'zhp_harc_sim_2003':
-        return SprawBookSlug.zhpHarcSim2003;
+        return zhpHarcSim2003;
       case 'zhp_harc_sim_2003_wodne':
         return SprawBookSlug.zhpHarcSim2003Wodne;
       default:
@@ -56,19 +56,19 @@ enum SprawBookSlug {
 
   OrgColors get colors{
     switch(this){
-      case SprawBookSlug.zhpHarcSim2003:
+      case zhpHarcSim2003:
         return OrgColors.outOfDate;
-      case SprawBookSlug.zhpHarcSim2003Wodne:
+      case zhpHarcSim2003Wodne:
         return OrgColors.outOfDate;
-      case SprawBookSlug.zhrHarcDSim2006:
+      case zhrHarcDSim2006:
         return OrgColors.outOfDate;
-      case SprawBookSlug.zhpZuchSim2022:
+      case zhpZuchSim2022:
         return Org.zhpZuch.colors;
-      case SprawBookSlug.zhpHarcSim2022:
+      case zhpHarcSim2022:
         return Org.zhp.colors;
-      case SprawBookSlug.zhrHarcCSim2023:
+      case zhrHarcCSim2023:
         return Org.zhrChlop.colors;
-      case SprawBookSlug.zhrHarcDSim2023:
+      case zhrHarcDSim2023:
         return Org.zhrDziew.colors;
     }
   }
@@ -94,19 +94,19 @@ enum SprawBookSlug {
 
   Org get org{
     switch(this){
-      case SprawBookSlug.zhpHarcSim2003:
+      case zhpHarcSim2003:
         return Org.zhp;
-      case SprawBookSlug.zhpHarcSim2003Wodne:
+      case zhpHarcSim2003Wodne:
         return Org.zhp;
-      case SprawBookSlug.zhrHarcDSim2006:
+      case zhrHarcDSim2006:
         return Org.zhrDziew;
-      case SprawBookSlug.zhpZuchSim2022:
+      case zhpZuchSim2022:
         return Org.zhpZuch;
-      case SprawBookSlug.zhpHarcSim2022:
+      case zhpHarcSim2022:
         return Org.zhp;
-      case SprawBookSlug.zhrHarcCSim2023:
+      case zhrHarcCSim2023:
         return Org.zhrChlop;
-      case SprawBookSlug.zhrHarcDSim2023:
+      case zhrHarcDSim2023:
         return Org.zhrDziew;
     }
   }
