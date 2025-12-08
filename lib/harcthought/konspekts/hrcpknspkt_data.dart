2bd20faf7e0918d2a85f1a42879f1f91ee9209f3
daniel <daniel.iwanicki@armlab.ai>
2025-12-08 16:10:16 +0100

    Fixes

--- lib/harcthought/konspekts/hrcpknspkt_data.dart
+++ lib/harcthought/konspekts/hrcpknspkt_data.dart
@@ -36,6 +36,7 @@ class AttachmentData {
 
     return {
       'name': name,
+      'formats': {...fileData.keys, ...urlData.keys}.map((e) => e.name).toList(),
       'files': filesJson,
       'urls': urlsJson,
       'printInfoEnabled': printInfoEnabled,
