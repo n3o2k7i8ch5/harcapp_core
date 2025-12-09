7bec129192ff1ea370ccffda7d70f461c69349d9
daniel <daniel.iwanicki@armlab.ai>
2025-12-09 09:37:14 +0100

    Fixes

--- lib/comm_widgets/blur.dart
+++ lib/comm_widgets/blur.dart
@@ -8,11 +8,13 @@ class Blur extends StatelessWidget{
   final double sigma;
   final TileMode mode;
   final BorderRadiusGeometry? borderRadius;
-  const Blur({this.child, this.sigma=8.0, this.mode = TileMode.repeated, this.borderRadius, super.key});
+  final Clip clipBehavior;
+  const Blur({this.child, this.sigma=8.0, this.mode = TileMode.repeated, this.borderRadius, this.clipBehavior = Clip.antiAlias, super.key});
 
   @override
   Widget build(BuildContext context) => ClipRRect(
       borderRadius: borderRadius ?? BorderRadius.zero,
+      clipBehavior: clipBehavior,
       child: BackdropFilter(
           filter: ImageFilter.blur(sigmaX: sigma, sigmaY: sigma, tileMode: mode),
           child: child??Container(color: Colors.transparent)
