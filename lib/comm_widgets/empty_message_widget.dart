4cc732a3da055fcf198d33cfc96df350c76fa64c
daniel <daniel.iwanicki@armlab.ai>
2025-12-13 10:50:04 +0100

    Fixes

---
+++ lib/comm_widgets/empty_message_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:harcapp_core/comm_classes/color_pack.dart';
+import 'package:harcapp_core/comm_classes/app_text_style.dart';
+import 'package:harcapp_core/values/dimen.dart';
+
+class EmptyMessageWidget extends StatelessWidget{
+
+  final String? text;
+  final IconData icon;
+  final Color? color;
+  final double size;
+
+  const EmptyMessageWidget({
+    required this.text,
+    required this.icon,
+    this.color,
+    this.size = 92, //Dimen.ICON_EMPTY_INFO_SIZE,
+    super.key});
+
+  @override
+  Widget build(BuildContext context) => Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    mainAxisSize: MainAxisSize.min,
+    children: [
+
+      Icon(icon, color: color??hintEnab_(context), size: size),
+      SizedBox(height: .1*size),
+      Text(
+        text!,
+        style: AppTextStyle(
+            color: color??hintEnab_(context),
+            fontSize: (Dimen.textSizeBig/Dimen.iconEmptyInfoSize)*size,
+            fontWeight: weightBold
+        ),
+        textAlign: TextAlign.center,
+        overflow: TextOverflow.ellipsis,
+      ),
+
+    ],
+  );
+
+}
