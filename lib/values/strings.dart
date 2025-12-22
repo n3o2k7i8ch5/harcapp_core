8ea7f5dd89f17725641321f8ab0349b675d6d81f
daniel <daniel.iwanicki@armlab.ai>
2025-12-22 19:50:57 +0100

    Fixes

--- lib/values/strings.dart
+++ lib/values/strings.dart
@@ -1,1 +1,18 @@
 const String noInternetMessage = 'Nie ma neta';
+
+String get simpleErrorMessage{
+
+  List<String> messages = [
+    'Coś poszło nie tak',
+    'Coś nie siadło',
+    'Coś nie hula',
+    'Coś nie styka',
+    'Coś nie bangla',
+    'Coś nie pykło',
+    'Coś jest nie teges',
+    'Coś tu nie gra',
+  ];
+  messages.shuffle();
+  return messages[0];
+
+}
