076d7e6bb74a66082bdf1a8fd1258879542fe44a
daniel <daniel.iwanicki@armlab.ai>
2025-12-23 14:48:54 +0100

    Fixes

--- lib/harcthought/games/instruction_row_widget.dart
+++ lib/harcthought/games/instruction_row_widget.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:harcapp_core/comm_classes/color_pack.dart';
 import 'package:harcapp_core/comm_widgets/app_card.dart';
 import 'package:harcapp_core/comm_widgets/blur.dart';
+import 'package:harcapp_core/comm_widgets/bottom_sheet.dart';
+import 'package:harcapp_core/comm_widgets/dialog/dialog.dart';
 import 'package:harcapp_core/comm_widgets/simple_button.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
@@ -14,6 +17,24 @@ class InstructionRowWidget extends StatelessWidget{
     this.onRulesTap,
     this.onGuideTap,
   });
+
+  static InstructionRowWidget simple({
+    required Widget Function(BuildContext context) rulesWidgetBuilder,
+    required Widget Function(BuildContext context) guideWidgetBuilder,
+  }) => InstructionRowWidget(
+    onRulesTap: (context) => showScrollBottomSheet(
+      context: context,
+      builder: (context) => BottomSheetBlur(
+        title: 'Zasady gry',
+        color: background_(context).withValues(alpha: 0.8),
+        builder: rulesWidgetBuilder
+      ),
+    ),
+    onGuideTap: (context) => openDialog(
+      context: context,
+      builder: guideWidgetBuilder,
+    ),
+  );
 
   @override
   Widget build(BuildContext context) => Row(
