ddfd7bad4fe82cd7798f2efad950198dcb0406cc
daniel <daniel.iwanicki@armlab.ai>
2025-12-23 09:38:18 +0100

    Fixes

--- lib/comm_widgets/bottom_sheet.dart
+++ lib/comm_widgets/bottom_sheet.dart
@@ -9,6 +9,8 @@ import 'package:harcapp_core/comm_classes/app_text_style.dart';
 import 'package:harcapp_core/comm_classes/common.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
+import 'blur.dart';
+
 
 Future<void> showScrollBottomSheet({required BuildContext context, required WidgetBuilder builder, bool scrollable = true}) async {
 
@@ -24,35 +26,13 @@ Future<void> showScrollBottomSheet({required BuildContext context, required Widg
     // Safe Area looks stupid here, since `useSafeArea: true` is used above, but it's needed to avoid the system
     // navigation bar overlapping the `BottomSheetTemplate`.
     builder: (context) => SafeArea(
-      child: BottomSheetTemplate(
-          builder: builder,
-          scrollable: scrollable
-      ),
+      child: LayoutBuilder(
+          builder: (context, constraints) => _BottomSheetContent(
+            content: builder(context),
+            maxHeight: constraints.maxHeight,
+          )
+      )
     ),
-  );
-}
-
-class BottomSheetTemplate extends StatelessWidget {
-
-  final WidgetBuilder builder;
-  final bool? scrollable;
-
-  const BottomSheetTemplate({
-    required this.builder,
-    this.scrollable,
-    super.key
-  });
-
-  @override
-  Widget build(BuildContext context) => LayoutBuilder(
-    builder: (context, constraints) {
-      final content = builder(context);
-
-      return _BottomSheetContent(
-        content: content,
-        maxHeight: constraints.maxHeight,
-      );
-    },
   );
 }
 
@@ -130,9 +110,92 @@ class _BottomSheetContentState extends State<_BottomSheetContent> {
   }
 }
 
-class BottomSheetDef extends StatefulWidget{
+class _BottomSheetRawWidget extends StatelessWidget{
 
   static const double radius = 12.0;
+  static const BorderRadius borderRadius = BorderRadius.only(
+      topLeft: Radius.circular(radius),
+      topRight: Radius.circular(radius)
+  );
+
+  final String? title;
+  final String? subTitle;
+  final Widget? Function(BuildContext context) builder;
+  final Color? textColor;
+  final Color? color;
+  final Color? colorEnd;
+  final EdgeInsets childMargin;
+
+  const _BottomSheetRawWidget({
+    this.title,
+    this.subTitle,
+    required this.builder,
+    this.textColor,
+    this.color,
+    this.colorEnd,
+    this.childMargin = const EdgeInsets.all(Dimen.BOTTOM_SHEET_MARG),
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context){
+
+    Widget _widget = Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        if(title!=null)
+          Padding(
+              padding: const EdgeInsets.all(Dimen.BOTTOM_SHEET_TITLE_MARG - Dimen.iconMarg),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(title!, style: AppTextStyle(fontWeight: weightHalfBold, color: textColor, fontSize: Dimen.textSizeBig), textAlign: TextAlign.end,),
+                      if(subTitle!=null) Text(subTitle!, style: AppTextStyle(color: hintEnab_(context), fontSize: Dimen.textSizeNormal), textAlign: TextAlign.end,),
+                    ],
+                  ),
+
+                  AppButton(
+                    icon: Icon(MdiIcons.close, color: textColor),
+                    onTap: () => Navigator.pop(context),
+                  )
+
+                ],
+              )
+          ),
+        Padding(
+          padding: childMargin,
+          child: builder(context),
+        ),
+      ],
+    );
+
+    if(colorEnd == null){
+      return Material(
+        borderRadius: borderRadius,
+        color: color??background_(context),
+        child: _widget,
+      );
+    }
+
+    return GradientWidget(
+      borderRadius: borderRadius,
+      colorStart: color??background_(context),
+      colorEnd: colorEnd??color??background_(context),
+      child: _widget,
+    );
+  }
+
+}
+
+class BottomSheetDef extends StatelessWidget{
+
+  static const double radius = _BottomSheetRawWidget.radius;
 
   final String? title;
   final String? subTitle;
@@ -150,63 +213,66 @@ class BottomSheetDef extends StatefulWidget{
     this.color,
     this.colorEnd,
     this.childMargin = const EdgeInsets.all(Dimen.BOTTOM_SHEET_MARG),
-    super.key});
-
-  @override
-  State<StatefulWidget> createState() => BottomSheetDefState();
-
-}
-
-class BottomSheetDefState extends State<BottomSheetDef>{
+    super.key
+  });
 
   @override
   Widget build(BuildContext context) => Padding(
-    padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-    child: GradientWidget(
-        borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(12.0),
-            topRight: Radius.circular(12.0)
-        ),
-        colorStart: widget.color??background_(context),
-        colorEnd: widget.colorEnd??widget.color??background_(context),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            if(widget.title!=null)
-              Padding(
-                  padding: const EdgeInsets.all(Dimen.BOTTOM_SHEET_TITLE_MARG - Dimen.iconMarg),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(widget.title!, style: AppTextStyle(fontWeight: weightHalfBold, color: widget.textColor, fontSize: Dimen.textSizeBig), textAlign: TextAlign.end,),
-                          if(widget.subTitle!=null) Text(widget.subTitle!, style: AppTextStyle(color: hintEnab_(context), fontSize: Dimen.textSizeNormal), textAlign: TextAlign.end,),
-                        ],
-                      ),
-
-                      AppButton(
-                        icon: Icon(MdiIcons.close, color: widget.textColor),
-                        onTap: () => Navigator.pop(context),
-                      )
-
-                    ],
-                  )
-              ),
-            Padding(
-              padding: widget.childMargin,
-              child: widget.builder(context),
-            ),
-          ],
-        ),
-
-    ),
+      padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: _BottomSheetRawWidget(
+        title: title,
+        subTitle: subTitle,
+        builder: builder,
+        textColor: textColor,
+        color: color,
+        colorEnd: colorEnd,
+        childMargin: childMargin,
+      )
   );
 
-  void notify() => setState((){});
+}
+
+class BottomSheetBlur extends StatelessWidget{
+
+  static const double radius = _BottomSheetRawWidget.radius;
+
+  final String? title;
+  final String? subTitle;
+  final Widget? Function(BuildContext context) builder;
+  final Color? textColor;
+  final Color? color;
+  final Color? colorEnd;
+  final EdgeInsets childMargin;
+  final double blur;
+
+  const BottomSheetBlur({
+    this.title,
+    this.subTitle,
+    required this.builder,
+    this.textColor,
+    this.color,
+    this.colorEnd,
+    this.childMargin = const EdgeInsets.all(Dimen.BOTTOM_SHEET_MARG),
+    this.blur = 10.0,
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context) => Padding(
+      padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Blur(
+        sigma: blur,
+        borderRadius: _BottomSheetRawWidget.borderRadius,
+        child: _BottomSheetRawWidget(
+          title: title,
+          subTitle: subTitle,
+          builder: builder,
+          textColor: textColor,
+          color: color,
+          colorEnd: colorEnd,
+          childMargin: childMargin,
+        )
+      )
+  );
 
 }
