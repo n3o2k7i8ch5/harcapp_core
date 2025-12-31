f0c1cb9a10e69d266805b0cb47049c3434a67650
daniel <daniel.iwanicki@armlab.ai>
2025-12-31 14:04:46 +0100

    Fixes

--- lib/comm_widgets/app_dropdown.dart
+++ lib/comm_widgets/app_dropdown.dart
@@ -41,7 +41,6 @@ class AppDropdown<T extends IconTextEnum> extends StatelessWidget{
   final void Function(T item) onSelected;
   final List<AppDropdownButton<T>> Function(BuildContext context) itemBuilder;
   final BorderRadius? borderRadius;
-  final List<T>? allValues;
 
   const AppDropdown({
     super.key,
@@ -54,47 +53,23 @@ class AppDropdown<T extends IconTextEnum> extends StatelessWidget{
     required this.onSelected,
     required this.itemBuilder,
     this.borderRadius,
-    this.allValues,
   });
 
-  double? _calculateMinWidth() {
-    if (allValues == null || allValues!.isEmpty) return null;
-    
-    double maxTextWidth = 0;
-    for (final value in allValues!) {
-      final textPainter = TextPainter(
-        text: TextSpan(text: value.text, style: AppTextStyle()),
-        maxLines: 1,
-        textDirection: TextDirection.ltr,
-      )..layout();
-      if (textPainter.width > maxTextWidth) {
-        maxTextWidth = textPainter.width;
-      }
-    }
-    // Add padding for icon (24) + icon margin (8) + button padding (2*12)
-    return maxTextWidth + 24 + 8 + 24;
-  }
-
   @override
-  Widget build(BuildContext context) {
-    final minWidth = constraints?.minWidth ?? _calculateMinWidth();
-    
-    return PopupMenuButton<T>(
-      shape: RoundedRectangleBorder(
-        borderRadius: borderRadius ?? BorderRadius.circular(AppCard.bigRadius),
-      ),
-      menuPadding: EdgeInsets.zero,
-      padding: EdgeInsets.zero,
-      clipBehavior: Clip.hardEdge,
-      color: color ?? background_(context),
-      icon: icon,
-      child: child,
-      position: position,
-      offset: offset ?? Offset.zero,
-      constraints: minWidth != null ? BoxConstraints(minWidth: minWidth) : null,
-      onSelected: onSelected,
-      itemBuilder: (BuildContext context) => itemBuilder.call(context)
-    );
-  }
+  Widget build(BuildContext context) => PopupMenuButton<T>(
+    shape: RoundedRectangleBorder(
+      borderRadius: borderRadius ?? BorderRadius.circular(AppCard.bigRadius),
+    ),
+    menuPadding: EdgeInsets.zero,
+    padding: EdgeInsets.zero,
+    clipBehavior: Clip.hardEdge,
+    color: color ?? background_(context),
+    position: position,
+    offset: offset ?? Offset.zero,
+    constraints: constraints,
+    onSelected: onSelected,
+    itemBuilder: (BuildContext context) => itemBuilder.call(context),
+    child: icon ?? child,
+  );
 
 }
