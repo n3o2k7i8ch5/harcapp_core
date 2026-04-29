ba98cf420cacb3eaf0930711fbefa1dd261bd251
daniel <daniel.iwanicki@armlab.ai>
2026-04-29 22:26:08 +0200

    Fixes

--- lib/folder/folder_tab.dart
+++ lib/folder/folder_tab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:harcapp_core/comm_classes/app_text_style.dart';
-import 'package:harcapp_core/comm_widgets/folder_icon.dart';
+
+import 'folder_icon.dart';
 
 class FolderBaseTab extends StatelessWidget implements PreferredSizeWidget{
 
